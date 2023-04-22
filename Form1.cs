a80eb103036064d670b091f5518caf85ebed3778
Mihail Sevagin <lolvillian@gmail.com>
2023-04-22 12:17:08 +0300

    Added calc functions

--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,33 @@
+using System.Diagnostics;
+
 namespace FuelCalcWF
 {
     public partial class Form1 : Form
     {
+        private double _distance = 0;
+        private double _value = 0;
+        private double _Price = 0;
+        private double _FuelVolume = 0;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void RenderWindow(object sender, EventArgs e)
+        {
+            Label price = new Label();
+            Label distance = new Label();
+            Label value = new Label();
+        }
+
+        private void FuelVolume()
+        {
+            _FuelVolume =  _distance * (_value / 100);
+        }
+
+        private double PriceTrip()
+        {
+            return _FuelVolume * _Price;
         }
     }
 }
